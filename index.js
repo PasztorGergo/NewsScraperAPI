11a5fd32c4d05330cf1cf747280f29dbf12ffb43
PasztorGergo <pasztorg05@gmail.com>
2022-05-14 15:38:49 +0200

    Querying news

--- index.js
+++ index.js
@@ -4,15 +4,33 @@ import * as cheerio from "cheerio";
 
 const app = express();
 const PORT = 5000;
-const url = "https://www.cnbc.com/business";
+const article = [];
 
-axios(url).then((res) => {
+const scraper = async (title) => {
+  if (article.length > 0) article.splice(0, article.length);
+  const res = await axios("https://www.cnbc.com/business");
   const html = res.data;
-  console.log(html);
-  cheerio.load(html);
-});
+  const $ = cheerio.load(html);
+  $(".Card-titleContainer", html).each((item, el) => {
+    article.push({
+      title: $(el).text(),
+      href: $(el).find("a").attr("href"),
+    });
+  });
+};
 app.get("/", (req, res) => {
-  res.send("Welcome to my webscraper!");
+  res.send("Welcome!");
+});
+
+app.get("/news", async (req, res) => {
+  await scraper();
+  res.send(article);
+});
+
+app.get("/news/:title", async (req, res) => {
+  const { title } = req.params;
+  await scraper();
+  res.send(article.filter((item) => item.title.includes(title)));
 });
 
 app.listen(PORT, () =>
